5d547f9faa006624fad76df67223dcb1a34cdd41
Josiah De La Torre <jodelat@yahoo.com>
2017-10-05 20:12:32 -0700

    routes work for different components to grab steam information

--- client/src/components/pages/FriendPage.js
+++ client/src/components/pages/FriendPage.js
@@ -42,9 +42,36 @@ class Results extends Component {
     })
   };
 
+  windowLocationHome = () => {
+    window.location.href = "/app#" + queryInput;
+  };
+
+  windowLocationFriends = () => {
+    window.location.href = "/friends#" + queryInput;
+  };
+
     render() {
       return (
         <div>
+        <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
+        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <a className="navbar-brand" href="#">Ghostly</a>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link" href="#" onClick={this.windowLocationHome}>Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#" onClick={this.windowLocationFriends}>Friends</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">Logout</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
         <Grid>
           <Row>
               {this.state.skills ? (
